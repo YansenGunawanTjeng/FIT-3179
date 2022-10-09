1779d557550e661e7eddd7534a8679e620ce4672
YansenGunawanTjeng <111474045+YansenGunawanTjeng@users.noreply.github.com>
2022-10-10 00:44:45 +1100

    Add files via upload

---
+++ Week 10 Homework/newcode2.js
@@ -0,0 +1,5 @@
+var code2 = "newcode2.json";
+vegaEmbed("#bar_chart", code2).then(function(result) {
+// Access the Vega view instance
+(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
