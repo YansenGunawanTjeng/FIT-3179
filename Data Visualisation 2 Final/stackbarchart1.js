f6d302cca2d4bf84db64df358ba3fd99c2b550ee
YansenGunawanTjeng <111474045+YansenGunawanTjeng@users.noreply.github.com>
2022-10-20 20:22:37 +1100

    Add files via upload

---
+++ Data Visualisation 2 Final/stackbarchart1.js
@@ -0,0 +1,5 @@
+var bar = "stackbarchart1.json";
+vegaEmbed("#bar_chart", bar).then(function(result) {
+// Access the Vega view instance
+(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
