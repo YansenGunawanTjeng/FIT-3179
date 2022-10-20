f4a4a377de1c709aada97300f1deab646ab623db
YansenGunawanTjeng <111474045+YansenGunawanTjeng@users.noreply.github.com>
2022-10-20 20:22:12 +1100

    Delete Data Visualisation 2 Final directory

--- Data Visualisation 2 Final/stackbarchart2.js
+++
@@ -1,5 +0,0 @@
-var bar2 = "stackbarchart2.json";
-vegaEmbed("#bar_chart", bar2).then(function(result) {
-// Access the Vega view instance
-(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
